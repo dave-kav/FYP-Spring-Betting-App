b9edeabf9a44dd6a44479e4f965759b8711ccba8
dave-kav <david.j.kavanagh@mycit.ie>
2017-03-24 21:06:18 +0000

    experimenting with ajax calls to ocr api

--- src/main/resources/static/scripts/ocr.js
+++ src/main/resources/static/scripts/ocr.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
-	var file = $('#bet-img')[0].src;
-//	.split("data:image/jpeg;base64,").pop();
+	var file = $('#bet-img')[0].src.split("data:image/jpeg;base64,").pop();
 	$.ajax({
 		type: 'POST',
 		url: 'https://ec2-34-250-24-17.eu-west-1.compute.amazonaws.com:443/ocr',
