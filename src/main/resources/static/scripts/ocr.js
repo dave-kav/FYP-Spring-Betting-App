ea487440b9e67370055cc4b83304b42a6108bbbd
dave-kav <david.j.kavanagh@mycit.ie>
2017-03-25 18:50:14 +0000

    successfully ajax 'GET' to ocr server and populating fields with response. need to change to post and accept image in some form

--- src/main/resources/static/scripts/ocr.js
+++ src/main/resources/static/scripts/ocr.js
@@ -1,12 +1,18 @@
 $(document).ready(function(){
-	var file = $('#bet-img')[0].src.split("data:image/jpeg;base64,").pop();
+	var thing = $("#loading").dialog('Analyzing betting slip...');
+	var file = $('#bet-img')[0].src;
+//	.split("data:image/jpeg;base64,").pop();
 	$.ajax({
-		type: 'POST',
-		url: 'https://ec2-34-250-24-17.eu-west-1.compute.amazonaws.com:443/ocr',
-		data: JSON.stringify({'img_data':file}),
+		type: 'GET',
+		url: 'http://ec2-34-250-24-17.eu-west-1.compute.amazonaws.com:8080/ocr',
+//		data: JSON.stringify({'img_data':file}),
 		dataType: 'json',
 		success: function(result) {
-			console.log(result);
+			$('#selection').val(result.selection);
+			$('#time').val(result.time);
+			$('#track').val(result.track);
+			$('#odds').val(result.odds);
+			thing = $.dialog("close");
 		},
 		error: function() {
 			console.log("didn't work");
